6c37bc0ef81bcd0a8cafcc442715688baf857e9a
mcabrera <mcabrera@github.com>
2012-08-29 17:06:10 -0300

    First commit

---
+++ src/Tests/Builditgreensampletext.java
@@ -0,0 +1,57 @@
+package Tests;
+
+import java.io.File;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Builditgreensampletext {
+	private WebDriver driver;
+	private String baseUrl;
+	private StringBuffer verificationErrors = new StringBuffer();
+	@Before
+	public void setUp() throws Exception {
+		
+		String xport=System.getProperty("lmportal.xvfb.id", ":1");
+		final File firefoxPath = new File(System.getProperty(
+                "lmportal.deploy.firefox.path", "/opt/firefox/"));
+        FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
+        firefoxBinary.setEnvironmentProperty("DISPLAY", xport);
+		
+		driver = new FirefoxDriver();
+		baseUrl = "http://builditgreen.org/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testBuilditgreensampletext() throws Exception {
+		driver.get(baseUrl);
+		driver.findElement(By.linkText("Contact")).click();
+		driver.findElement(By.name("Submit")).click();
+		assertTrue(driver.getPageSource().contains("Please re-enter the code displayed in the image below. "));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+}
